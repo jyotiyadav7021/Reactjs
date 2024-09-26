e0c7fc199b0f7d6a5036519745ad0d44dc3ce840
JyotiYadav <57616116+jyotiyadav7021@users.noreply.github.com>
2024-09-26 17:30:31 +0530

    new

---
+++ clock/src/App.jsx
@@ -0,0 +1,14 @@
+
+import './App.css'
+import Poster from './components/poster'
+function App() {
+  
+
+  return (
+    <>
+      <h1 text-3xl font-bold underline >hello world</h1>
+    </>
+  )
+}
+
+export default App
---
+++ clock/src/components/Poster.jsx
@@ -0,0 +1,11 @@
+function Poster() {
+  
+
+    return (
+      <>
+        <h1 text-3xl font-bold underline >My team member</h1>
+      </>
+    )
+  }
+  
+  export default Poster
